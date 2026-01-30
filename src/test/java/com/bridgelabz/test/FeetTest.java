fe34f36eda5183d5e42fbe3e17645520a00fd149
Upendra <upendrab33@gmail.com>
2026-01-30 12:37:25 +0530

    test file name has been changed

--- src/test/java/com/bridgelabz/test/FeetTest.java
+++ src/test/java/com/bridgelabz/test/FeetTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class QuantityMeasurementAppTest {
+public class FeetTest {
 
     @Test
     public void testEqualitySameValue(){
